4c3041e68f55dc8df29322d95b092ef01aac24f1
Guilherme Salim <42344215+KaiFlame@users.noreply.github.com>
2020-05-04 15:24:59 -0500

    salim

---
+++ Cod/salim.asm
@@ -0,0 +1,41 @@
+    .486                                    ; create 32 bit code
+    .model flat, stdcall                    ; 32 bit memory model
+    option casemap :none                    ; case sensitive
+ 
+    include \masm32\include\windows.inc     ; always first
+    include \masm32\macros\macros.asm       ; MASM support macros
+    include \masm32\include\masm32.inc
+    include \masm32\include\gdi32.inc
+    include \masm32\include\user32.inc
+    include \masm32\include\kernel32.inc
+    
+    includelib \masm32\lib\masm32.lib
+    includelib \masm32\lib\gdi32.lib
+    includelib \masm32\lib\user32.lib
+    includelib \masm32\lib\kernel32.lib
+    
+.data?
+
+.data
+
+
+
+texto  db 0,0     ; variável usada para printar na tela
+
+
+.code                       ; Tell MASM where the code starts
+start: 
+
+     mov  ax, dx
+    xor  dx, dx
+    mov  cx, 10    
+    div  cx       ; here dx contains the remainder of the division - from 0 to 9
+
+    add  dl, '0'  ; to ascii from '0' to '9'
+    mov edx, OFFSET texto
+    print edx
+
+end start
+
+
+
